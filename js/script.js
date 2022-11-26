26cf931934f74b648b93aba0a0c879e9c71d239b
a-rede <abell.rede1@outlook.com>
2022-11-25 21:53:28 -0800

    Add functionality to new note button where it reveals the hidden elements

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 //* Dark button function
 const darkButton = document.querySelector('.dark-btn');
+darkButton.addEventListener('click', darkThemeToggler);
 
-darkButton.addEventListener('click', () => {
+function darkThemeToggler() {
     document.body.classList.toggle('dark-mode');
     if (darkButton.textContent === 'Dark Theme') {
         darkButton.textContent = 'Light Theme';
     } else if (darkButton.textContent === 'Light Theme') {
         darkButton.textContent = 'Dark Theme';
     };
-});
+};
 
 //* Cancel button function
 const cancelButton = document.querySelector('.cancel-btn');
 
 cancelButton.addEventListener('click', () => {
-    let cancelButton = document.querySelector('.bot-btn').classList.add('hidden');
-    let textArea = document.querySelector('#note-input').classList.add('hidden');
+    document.querySelector('.bot-btn').classList.add('hidden');
+    document.querySelector('#note-input').classList.add('hidden');
 });
 
-console.log(cancelButton);
 //* New Note button function
+const newNoteButton = document.querySelector('.top-btn > button');
 
-
+newNoteButton.addEventListener('click', () => {
+    document.querySelector('.bot-btn').classList.remove('hidden');
+    document.querySelector('#note-input').classList.remove('hidden');
+});
