d017c0cd38bbbaa64e228b1732566505fd5e41a2
a-rede <abell.rede1@outlook.com>
2022-11-30 05:39:09 -0800

    Add the ability to save notes to the my notes sidebar using the save button

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ darkButton.addEventListener("click", darkThemeToggler);
 
 function darkThemeToggler() {
     document.body.classList.toggle("dark-mode");
+    changeBtnText();
+}
+
+function changeBtnText() {
     if (darkButton.textContent === "Dark Theme") {
         darkButton.textContent = "Light Theme";
     } else if (darkButton.textContent === "Light Theme") {
@@ -47,24 +51,27 @@ let notesArray = [
     },
 ];
 
+//* Save button function
 const saveButton = document.querySelector(".bot-btn > button");
+const notesList = document.querySelector('.my-notes-list');
 saveButton.addEventListener("click", saveNotes);
 
 function saveNotes() {
     let noteTitle = prompt("Enter note title:");
     let noteBody = document.querySelector("#note-input").value;
+
+    // Saves note information in an array of objects
     notesArray.push({
         title: noteTitle,
         body: noteBody,
     });
+    updateMyNotes();
 };
 
-
-// saveButton.addEventListener("click", () => {
-//     let noteTitle = prompt("Enter note title:");
-//     let note = document.querySelector("#note-input").value;
-//     notesArray.push({
-//         title: noteTitle,
-//         body: note,
-//     });
-// });
+function updateMyNotes() {
+    const listItem = document.createElement('li');
+    for (let note of notesArray) {
+        listItem.textContent = `${note.title}`;
+    }
+    notesList.append(listItem);
+}
